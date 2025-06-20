de683972f9cc466ab950c236463528b4da2fb475
Leander Furumo <leander@furumo.eu>
2025-06-20 22:57:35 +0200

    run linter

--- src/app/components/GuessForm.tsx
+++ src/app/components/GuessForm.tsx
@@ -19,7 +19,9 @@ const GuessForm: React.FC<GuessFormProps> = ({ input, onInputChange, onSubmit, d
         value={input}
         onChange={onInputChange}
         className={`focus:border-accent-primary h-12 w-60 rounded border px-2 py-1 focus:outline-none md:w-75 ${
-          disabled ? "cursor-not-allowed border-gray-500 bg-gray-400 opacity-50" : "bg-background-secondary duration-300"
+          disabled
+            ? "cursor-not-allowed border-gray-500 bg-gray-400 opacity-50"
+            : "bg-background-secondary duration-300"
         }`}
         placeholder={t.placeholder}
         disabled={disabled}
