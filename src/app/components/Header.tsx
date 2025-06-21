5570fc8eb0ee62ae4315762b88c190f00447edae
Leander Furumo <leander@furumo.eu>
2025-06-22 00:36:15 +0200

    increase top margin for header
    avoid collisions

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   const t = translations[language];
   return (
     <header>
-      <h1 className="mb-12 text-center text-4xl font-bold">
+      <h1 className="mt-24 mb-12 text-center text-4xl font-bold">
         {t.appTitle.split(" ")[0]} {GOAL_AMOUNT} <span className="border-accent-primary border-b-2">{t.subject}</span>
       </h1>
     </header>
