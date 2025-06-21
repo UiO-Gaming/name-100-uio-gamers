259484195f87bbdfdb66ad0b7e24aafcf2fd93b1
Leander Furumo <leander@furumo.eu>
2025-06-22 00:46:54 +0200

    use cursor-pointer on button hover

--- src/app/components/LoginButton.tsx
+++ src/app/components/LoginButton.tsx
@@ -7,7 +7,7 @@ export default function LoginButton({ text }: { text: string }) {
   return (
     <button
       onClick={() => signIn("discord")}
-      className="flex items-center gap-2 rounded-md bg-[#5865F2] px-4 py-2 text-white"
+      className="flex cursor-pointer items-center gap-2 rounded-md bg-[#5865F2] px-4 py-2 text-white"
     >
       <FaDiscord />
       {text}
--- src/app/components/LogoutButton.tsx
+++ src/app/components/LogoutButton.tsx
@@ -5,7 +5,11 @@ import { CiLogout } from "react-icons/ci";
 
 export default function LogoutButton() {
   return (
-    <button onClick={() => signOut()} className="rounded-lg bg-red-500 p-2 text-white" aria-label="Logout">
+    <button
+      onClick={() => signOut()}
+      className="cursor-pointer rounded-lg bg-red-500 p-2 text-white"
+      aria-label="Logout"
+    >
       <CiLogout size={24} />
     </button>
   );
