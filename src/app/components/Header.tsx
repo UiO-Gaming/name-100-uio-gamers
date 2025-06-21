8e92e2850f8b888e8baa51192fc8a6a468e972e1
Leander Furumo <leander@furumo.eu>
2025-06-21 21:20:57 +0200

    adapt translations for uio gaming version

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <header>
       <h1 className="mb-12 text-center text-4xl font-bold">
-        {t.appTitle.split(" ")[0]} {GOAL_AMOUNT} <span className="border-norway border-b-2">{t.norwegians}</span>
+        {t.appTitle.split(" ")[0]} {GOAL_AMOUNT} <span className="border-accent-primary border-b-2">{t.subject}</span>
       </h1>
     </header>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import "@/globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Name 100 Norwegians",
-  description: "How fast can you name 100 Norwegian people?",
+  title: "Name 100 UiO Gamers",
+  description: "How fast can you name 100 UiO Gamers?",
 };
 
 export default function RootLayout({
